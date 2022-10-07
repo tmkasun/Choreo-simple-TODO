d7a68257fa40180f0864aaf2e694e3a5c36cf3a2
Kasun Thennakoon <tmkasun@gmail.com>
2022-10-07 04:29:38 +0000

    fix tokenn temp issue

--- src/data/hooks/tasks.js
+++ src/data/hooks/tasks.js
@@ -11,19 +11,23 @@ export const TASK_STATUS = {
 
 const getRandomStatus = () => Object.values(TASK_STATUS)[Math.floor(Math.random() * (3))]
 
-const allTasks = (async () => {
+let tempData = null;
+const allTasks = async () => {
     const response = await fetch(`${API_BASE_URL}/todos`, { headers: { Authorization: `bearer ${getCookie('access_token')}` } });
     const data = await response.json();
     data.list = data.list.map(task => ({ ...task, status: getRandomStatus() }));
     return data;
-})()
+}
 
 export const useTasksByGroup = () => {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        allTasks.then(setData).finally(() => setIsLoading(false));
+        if(!tempData) {
+            tempData = allTasks();
+        }
+        tempData.then(setData).finally(() => setIsLoading(false));
     }, [])
 
     return { data, isLoading, setData };
