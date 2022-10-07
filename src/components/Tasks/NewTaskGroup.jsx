1da30f2a191b5566ee2ec4970e7bfbbf6d69c2ea
Kasun Thennakoon <tmkasun@gmail.com>
2022-10-07 07:04:02 +0000

    add components/Tasks/NewTaskGroup.jsx

---
+++ src/components/Tasks/NewTaskGroup.jsx
@@ -0,0 +1,12 @@
+import '../../styles/Tasks/NewTasksGroup.css'
+import { IconButton } from '../IconButton';
+
+
+export default function NewTaskGroup(props) {
+
+    return (
+        <div className='new-task-group-container'>
+            <span className='add-character' />
+        </div>
+    )
+}
