53ea04af012881d2659135df519522ac010a2549
Kasun Thennakoon <tmkasun@gmail.com>
2022-10-21 14:36:15 +0530

    update logout button

--- src/components/Header/AvatarMenu.jsx
+++ src/components/Header/AvatarMenu.jsx
@@ -1,30 +1,36 @@
 import React, { useState } from 'react';
 import useUser from '../../data/hooks/user.js';
-import '../../styles/AvatarMenu.css'
+import '../../styles/AvatarMenu.css';
 
 import { default as AsgardeoConfig } from '../../data/configs/asgardeo.json';
 const signOutRedirectURL = process.env.REACT_APP_signOutRedirectURL;
 
 export default function AvatarMenu() {
-    const [isHovered, setIsHovered] = useState(false);
     const [isInProgress, setIsInProgress] = useState(false);
     const [user] = useUser();
     const logoutHandler = () => {
         setIsInProgress(true);
         user.logout();
-        window.location.href = `${AsgardeoConfig.baseUrl}/oidc/logout?id_token_hint=${user.asgardeoIdToken}&post_logout_redirect_uri=${signOutRedirectURL}`
-    }
+        window.location.href = `${AsgardeoConfig.baseUrl}/oidc/logout?id_token_hint=${user.asgardeoIdToken}&post_logout_redirect_uri=${signOutRedirectURL}`;
+    };
 
     return (
-        <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='login-panel'>
-            <button disabled={isInProgress} className='avatar-button'>
-                <img className='user-avatar' src={user.picture} alt='User avatar' /> Hi, {user.given_name}
+        <div className="login-panel">
+            <button disabled={isInProgress} className="avatar-button">
+                <img
+                    className="user-avatar"
+                    src={user.picture}
+                    alt="User avatar"
+                />
+                Hi, {user.given_name}
             </button>
-            {user && isHovered && !isInProgress && <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='login-dropdown'>
-                <button className='logout-button' onClick={logoutHandler}>
-                    Logout
-                </button>
-            </div>}
+            {user && !isInProgress && (
+                <div className="dropdown-content avatar-dropdown">
+                    <a onClick={logoutHandler} href="#">
+                        Logout
+                    </a>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/Tasks/TasksGroup.jsx
+++ src/components/Tasks/TasksGroup.jsx
@@ -1,78 +1,98 @@
 import React, { useState } from 'react';
-import NewTask from "./NewTask";
-import TasksList from "./TasksList";
+import NewTask from './NewTask';
+import TasksList from './TasksList';
 
-import '../../styles/Tasks/TasksGroup.css'
+import '../../styles/Tasks/TasksGroup.css';
 import Dropdown from '../Dropdown';
 import { useDeleteGroup } from '../../data/hooks/groups';
 
 export const GROUP_ACTIONS = {
     EDIT: 'edit',
-    DELETE: 'delete'
-}
+    DELETE: 'delete',
+};
 
 function TasksGroup(props) {
     const { group, onGroupUpdate, movingTasks } = props;
     const { id, name } = group;
     const { tasks } = group;
-    const {deleteGroup} = useDeleteGroup();
+    const { deleteGroup } = useDeleteGroup();
     const [isEditing, setIsEditing] = useState(false);
     const onDelete = (deletedTask) => {
         onGroupUpdate((currentGroups) => {
-            const currentTasks = currentGroups.find(group => group.id === id).tasks;
-            const updatedTasks = currentTasks.filter(task => task.id !== deletedTask.id)
+            const currentTasks = currentGroups.find(
+                (group) => group.id === id
+            ).tasks;
+            const updatedTasks = currentTasks.filter(
+                (task) => task.id !== deletedTask.id
+            );
             return [id, updatedTasks];
-        })
-    }
+        });
+    };
     const onUpdate = (updatedTask) => {
         onGroupUpdate((currentGroups) => {
-            const currentTasks = currentGroups.find(group => group.id === id).tasks;
-            const updatedTasks = currentTasks.map(task => {
+            const currentTasks = currentGroups.find(
+                (group) => group.id === id
+            ).tasks;
+            const updatedTasks = currentTasks.map((task) => {
                 if (task.id === updatedTask.id) {
                     return updatedTask;
                 } else {
-                    return { ...task }
+                    return { ...task };
                 }
-            })
+            });
             return [id, updatedTasks];
-        })
-    }
+        });
+    };
     const onAdd = (newTask) => {
         onGroupUpdate((currentGroups) => {
-            const currentTasks = currentGroups.find(group => group.id === id).tasks;
+            const currentTasks = currentGroups.find(
+                (group) => group.id === id
+            ).tasks;
             return [id, [...currentTasks, newTask]];
-        })
-    }
+        });
+    };
 
     const onGroupDelete = (deletedGroupId) => {
-        onGroupUpdate(() => [deletedGroupId])
+        onGroupUpdate(() => [deletedGroupId]);
     };
 
     const handleGroupUpdate = (selection) => {
         setIsEditing(true);
-        if(selection === GROUP_ACTIONS.DELETE) {
-            deleteGroup(group.id, {onSuccess: onGroupDelete});
+        if (selection === GROUP_ACTIONS.DELETE) {
+            deleteGroup(group.id, { onSuccess: onGroupDelete });
         }
-        if(selection === GROUP_ACTIONS.EDIT) {
+        if (selection === GROUP_ACTIONS.EDIT) {
         }
-     }
+    };
     return (
-        <div className='task-group-container'>
-            {isEditing && <div className='group-deleting-overlay'>
-                Deleting . . .
-            </div>}
-            <div className='group-header'>
-                <div className='group-header-content'>
+        <div className="task-group-container">
+            {isEditing && (
+                <div className="group-deleting-overlay">Deleting . . .</div>
+            )}
+            <div className="group-header">
+                <div className="group-header-content">
                     <h5 className="task-group-name">{name}</h5>
                 </div>
-                {id > 12 && <Dropdown horizontal values={Object.values(GROUP_ACTIONS)} onChange={handleGroupUpdate} />}
+                {id > 12 && (
+                    <Dropdown
+                        horizontal
+                        values={Object.values(GROUP_ACTIONS)}
+                        onChange={handleGroupUpdate}
+                    />
+                )}
             </div>
-            <div className='new-todo-layout'>
+            <div className="new-todo-layout">
                 <NewTask groupId={id} onAdd={onAdd} />
             </div>
-            <TasksList movingTasks={movingTasks} groupId={id} onUpdate={onUpdate} onDelete={onDelete} tasks={tasks} />
+            <TasksList
+                movingTasks={movingTasks}
+                groupId={id}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+                tasks={tasks}
+            />
         </div>
-    )
+    );
 }
 
 export default React.memo(TasksGroup);
